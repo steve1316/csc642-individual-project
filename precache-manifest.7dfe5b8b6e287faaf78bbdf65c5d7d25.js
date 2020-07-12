bbb8f17228bb24460bd4d0ac68dd7ea6d7db6bb2
Steve Tu <stevetu11@yahoo.com>
2020-07-12 13:57:54 -0700

    Updates

---
+++ precache-manifest.7dfe5b8b6e287faaf78bbdf65c5d7d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1042df52f1420e22b024e50c687f1041",
+    "url": "/csc642-individual-project/index.html"
+  },
+  {
+    "revision": "9435218a0db265f5ab09",
+    "url": "/csc642-individual-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d478ae8bfc2bee5f2066",
+    "url": "/csc642-individual-project/static/js/2.6acad51b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/csc642-individual-project/static/js/2.6acad51b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9435218a0db265f5ab09",
+    "url": "/csc642-individual-project/static/js/main.4d2471e3.chunk.js"
+  },
+  {
+    "revision": "81f5ebe9921e0631b62b",
+    "url": "/csc642-individual-project/static/js/runtime-main.dc521708.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/csc642-individual-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
